85090c30ef02c2a27514d39118a621e8a3840098
Spacha <miikasikala96@gmail.com>
2022-04-05 15:02:43 +0300

    Added mode different modes and a simple test sequence.
    Bound separate pins for controlling the mode.

--- src/main/Programmer.h
+++ src/main/Programmer.h
@@ -14,15 +14,20 @@ namespace spc
     Pin nChipEnable;
     Pin nOutputEnable;
     //Pin modeSelect;
+    Pin modeProgram;
+    Pin modeErase;
     Pin data[DATA_BITS];
   };
+
+  enum Mode { MODE_READ, MODE_PROGRAM, MODE_ERASE };
 
   class EEPROMProgrammer
   {
   public:
     PinConfig pins;
+    Mode mode;
   
-    Programmer() { };
+    EEPROMProgrammer() { };
     
     void setPinConfig(PinConfig &pinConfig)
     {
@@ -33,13 +38,23 @@ namespace spc
      * Initialize programmer.
      */
     bool initialize()
-    {
+    { 
       pinMode(pins.addrData, OUTPUT);
       pinMode(pins.addrClock, OUTPUT);
       pinMode(pins.addrLatch, OUTPUT);
       pinMode(pins.nChipEnable, OUTPUT);
       pinMode(pins.nOutputEnable, OUTPUT);
       //pinMode(pins.modeSelect, OUTPUT);
+      pinMode(pins.modeProgram, OUTPUT);
+      pinMode(pins.modeErase, OUTPUT);
+
+      digitalWrite(pins.addrData, LOW);
+      digitalWrite(pins.addrClock, LOW);
+      digitalWrite(pins.addrLatch, LOW);
+
+      setAddress(0);  // reset the shift register
+      setMode(MODE_READ);
+      enableChip(true);
 
       for (auto pin : pins.data)
         pinMode(pin, INPUT);
@@ -48,7 +63,7 @@ namespace spc
     }
 
     /**
-     * Read byte from address.
+     * Read a byte from an address.
      */
     byte read(int16_t addr)
     {
@@ -61,6 +76,71 @@ namespace spc
     }
 
     /**
+     * Write abyte to an address.
+     * 
+     * NOTE: If writing multiple bytes, keep the mode as PROGRAM
+     *       (same for data pin modes) without changing them
+     *       between each byte!
+     */
+    void write(int16_t addr, byte data)
+    {
+      /**
+       * 1. Disable chip (nCE = HIGH)
+       * 2. Set programming mode (nOE = 12V).
+       * 3. Set address to the shift register and latch it out.
+       * 4. Set the desired data to the data pins.
+       * 5. Pulse nCE LOW.
+       */
+      enableChip(false);
+      setMode(MODE_PROGRAM);
+      setAddress(addr);
+
+      for (int pinNum = 0; pinNum < DATA_BITS; pinNum++)
+        digitalWrite(pins.data[pinNum], (data >> pinNum & 1));
+
+      pulseLow(pins.nChipEnable);
+      setMode(MODE_READ);
+    }
+
+    /**
+     * Set programmer mode (MODE_READ | MODE_PROGRAM | MODE_ERASE).
+     */
+    void setMode(Mode mode)
+    {
+      switch (mode)
+      {
+        case MODE_READ:
+          digitalWrite(pins.modeErase, LOW);
+          digitalWrite(pins.modeProgram, LOW);
+          digitalWrite(pins.nOutputEnable, LOW);
+          break;
+        case MODE_PROGRAM:
+          digitalWrite(pins.modeErase, LOW);
+          digitalWrite(pins.modeProgram, HIGH);
+          digitalWrite(pins.nOutputEnable, HIGH);
+          break;
+        case MODE_ERASE:
+          digitalWrite(pins.modeErase, HIGH);
+          digitalWrite(pins.modeProgram, LOW);
+          digitalWrite(pins.nOutputEnable, HIGH);
+          break;
+        default:
+          break;
+      }
+
+      mode = mode;
+    }
+
+    /**
+     * Enable or disable chip.
+     */
+    void enableChip(bool enable = true)
+    {
+      digitalWrite(pins.nChipEnable, enable ? LOW : HIGH);
+    }
+
+  protected:
+    /**
      * Set address to the chip.
      */
     void setAddress(uint16_t addr, bool set = true)
@@ -69,16 +149,29 @@ namespace spc
       shiftOut(pins.addrData, pins.addrClock, MSBFIRST, addr & 0xff);         // low byte
 
       if (set)
-        pulse(pins.addrLatch);
+        pulseHigh(pins.addrLatch);
     }
 
-  protected:
-    void pulse(Pin pin)
+    /**
+     * Pulse given pin to HIGH.
+     */
+    void pulseHigh(Pin pin)
     {
       digitalWrite(pin, LOW);
       digitalWrite(pin, HIGH);
-      delayMicroseconds(100);
+      delayMicroseconds(1);
       digitalWrite(pin, LOW);
+    }
+
+    /**
+     * Pulse given pin to LOW.
+     */
+    void pulseLow(Pin pin)
+    {
+      digitalWrite(pin, HIGH);
+      digitalWrite(pin, LOW);
+      delayMicroseconds(1);
+      digitalWrite(pin, HIGH);
     }
   };
 };
--- src/main/main.ino
+++ src/main/main.ino
@@ -13,6 +13,8 @@
 #define SHIFT_CLOCK 3  // SHCP
 #define SHIFT_LATCH 4  // STCP
 
+/* TODO: Introduce second serial for streaming bytes to/from data and address pins. */
+
 static const spc::Pin dataPins[DATA_BITS] = { 5, 6, 7, 8, 9, 10, 11, 12 };
 
 spc::EEPROMProgrammer programmer;
@@ -33,32 +35,74 @@ void setup()
   // set up the programmer
   spc::PinConfig programmerPins;
 
-  programmerPins.addrData = SHIFT_DATA;
-  programmerPins.addrClock = SHIFT_CLOCK;
-  programmerPins.addrLatch = SHIFT_LATCH;
-  programmerPins.nChipEnable = A0;
-  programmerPins.nOutputEnable = A1;
-  //programmerPins.modeSelect = A2;
+  programmerPins.addrData       = SHIFT_DATA;
+  programmerPins.addrClock      = SHIFT_CLOCK;
+  programmerPins.addrLatch      = SHIFT_LATCH;
+  programmerPins.nChipEnable    = A0;
+  programmerPins.nOutputEnable  = A1;
+  //programmerPins.modeSelect   = A2;
+  programmerPins.modeProgram    = A2;
+  programmerPins.modeErase      = A3;
   memcpy(programmerPins.data, dataPins, DATA_BITS);
   
   programmer.setPinConfig(programmerPins);
   if (!programmer.initialize())
   {
     Serial.println("Failed to initialize spc::EEPROMProgrammer.");
-    return 0;
+    return;
   }
+
+  Serial.println("Everything is in default state.");
+
+  delay(1000);
+  Serial.println("Enable chip");
+  digitalWrite(programmerPins.nChipEnable, LOW);
+
+  delay(5000);
+  Serial.println("Enable output");
+  digitalWrite(programmerPins.nOutputEnable, LOW);
+
+  delay(5000);
+  Serial.println("Set Mode: PROGRAM");
+  digitalWrite(programmerPins.modeErase, LOW);
+  digitalWrite(programmerPins.modeProgram, HIGH);
+
+  delay(5000);
+  Serial.println("Mode: ERASE");
+  digitalWrite(programmerPins.modeProgram, LOW);
+  digitalWrite(programmerPins.modeErase, HIGH);
+
+  delay(5000);
+  Serial.println("Disable chip, disable output, mode: READ.");
+  digitalWrite(programmerPins.nChipEnable, HIGH);
+  digitalWrite(programmerPins.nOutputEnable, HIGH);
+  digitalWrite(programmerPins.modeProgram, LOW);
+  digitalWrite(programmerPins.modeErase, LOW);
+  delay(10000);
+
+  Serial.println("*** Test done, go on. ***");
+  digitalWrite(programmerPins.nChipEnable, LOW);
+  digitalWrite(programmerPins.nOutputEnable, LOW);
+
+  return;
 }
 
 void loop()
 {
   // programmer.erase();
   // programmer.write(int16_t addr, int8_t data);
-  int16_t addr = 0x1234;
-  byte data = programmer.read(addr);
-
   char sbuf[32];
+  int16_t addr;
+  byte data;
+
+  addr = 0x1234;
+  data = programmer.read(addr);
   sprintf(sbuf, "0x%04x: %u", addr, data);
-  //Serial.println(sbuf);
+  Serial.println(sbuf);
+
+  addr = 0xBABE;
+  data = programmer.read(addr);
+  sprintf(sbuf, "0x%04x: %u", addr, data);
   Serial.println(sbuf);
 
   delay(500);
